a3d149ebb767635bdf9af27b0e9ce559f9d9f856
Norihiro Narayama <narayaman@gmail.com>
2022-05-26 17:48:12 +0900

    文字列絞り込みメソッド作成

--- src/components/LayerFilter/layer.ts
+++ src/components/LayerFilter/layer.ts
@@ -10,3 +10,14 @@ export const filterLayerName = (dataList: resource[], selectedResourceNameList: 
     const resourceName = resource.title;
     return selectedResourceNameList.includes(resourceName);
   });
+
+/**
+ * 絞り込み入力で絞り込まれたレイヤだけ収集する
+ * @param dataList
+ * @param selectedRefinementWord
+ */
+ export const filterLayerNameInputText = (dataList: resource[], selectedRefinementWord: string) =>
+ dataList.filter((resource) => {
+   const regExp = new RegExp(`.*(${selectedRefinementWord}).*`);
+   return resource.title.match(regExp);
+ });
