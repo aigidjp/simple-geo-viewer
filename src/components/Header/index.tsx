ef8d1d5c7f7053b9ddf11db8107ab566eb088ab4
DevKoukiMaeda <devkoukimaeda@gmail.com>
2022-06-24 13:20:04 +0900

    全体的な見た目の調整

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,8 +11,9 @@ function Header() {
   };
 
   return (
-    <header style={headerStyle} className="h-full flex justify-center items-center">
-      <div className="text-center text-white font-semibold text-3xl w-7/12">{getTitle()}</div>
+    <header style={headerStyle} className="h-full flex justify-left items-center">
+      <div className="text-left text-white font-semibold text-3xl w-7/12 p-3">{getTitle()}</div>
+      <div className="text-right text-white font-semibold text-3l w-7/12 p-3">Powerd By AIGID</div>
     </header>
   );
 }
