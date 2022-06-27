28df86a909d1a039f9311357bb0d8e082c1ec8bb
Kanahiro <mediterranean1769@gmail.com>
2022-06-27 20:32:15 +0900

    JSONに型を当てる

--- src/components/LayerFilter/config.ts
+++ src/components/LayerFilter/config.ts
@@ -1,16 +1,130 @@
 import configJson from '@/assets/config.json';
 import { getMenu, getFilteredIdList } from '@/components/LayerFilter/menu';
+import { RGBAColor } from 'deck.gl';
+
+type LayerGenericProps = {
+  id: string;
+  source: string;
+  minzoom: number;
+  maxzoom: number;
+  visible;
+};
+
+type RasterLayer = LayerGenericProps & {
+  type: 'raster';
+};
+
+type MvtLayer = LayerGenericProps & {
+  type: 'mvt';
+  getFillColor: RGBAColor;
+};
+
+type GeojsonLayer = LayerGenericProps & {
+  type: 'geojson';
+  getLineColor: RGBAColor;
+  lineWidthMinPixels: number;
+  getFillColor: RGBAColor;
+};
+
+type GeojsonIconLayer = LayerGenericProps & {
+  type: 'geojsonicon';
+  stroked: boolean;
+  filled: boolean;
+  icon: {
+    url: 'images/school_yellow.png';
+    width: 64;
+    height: 64;
+    anchorY: 64;
+  };
+  iconSizeScale: number;
+};
+
+type BustripLayer = LayerGenericProps & {
+  type: 'bus_trip';
+  iconUrl: string;
+};
+
+type Tile3dLayer = LayerGenericProps & {
+  type: '3dtiles';
+  pointsize: number;
+};
+
+type ScatterprotLayer = {
+  type: 'Scatterplot';
+  id: string;
+  data: string;
+  getLineColor: RGBAColor;
+  getFillColor: RGBAColor;
+  minzoom: number;
+  maxzoom: number;
+  visible: boolean;
+};
+
+type ArcLayer = {
+  type: 'Arc';
+  id: string;
+  data: string;
+  width: number;
+  sourceColor: RGBAColor;
+  targetColor: RGBAColor;
+  minzoom: number;
+  maxzoom: number;
+  visible: boolean;
+};
+
+type TemporalPolygonLayer = LayerGenericProps & {
+  type: 'temporal_polygon';
+  values: [number, number];
+  colors: [RGBAColor, RGBAColor];
+  heights: [number, number];
+  colorScale: number;
+};
+
+type TripsJsonLayer = LayerGenericProps & {
+  type: 'trips_json';
+  color: RGBAColor;
+  opacity: number;
+  trailLength: number;
+};
+
+type TripsDrmLayer = LayerGenericProps & {
+  type: 'trips_drm';
+  values: [number, number];
+  colors: [RGBAColor, RGBAColor];
+  step: number;
+};
+
+type Layer =
+  | RasterLayer
+  | MvtLayer
+  | GeojsonLayer
+  | GeojsonIconLayer
+  | BustripLayer
+  | Tile3dLayer
+  | ScatterprotLayer
+  | ArcLayer
+  | TemporalPolygonLayer
+  | TripsJsonLayer
+  | TripsDrmLayer;
+
+type Config = {
+  layers: Layer[];
+};
+
+const loadConfigJson = () => configJson as Config;
 
 /**
  * 表示可能なレイヤのconfigだけ収集する
  */
 export const getFilteredLayerConfig = () => {
-  return configJson['layers'].filter((layer) => getFilteredIdList(getMenu()).includes(layer.id));
+  const config = loadConfigJson();
+  return config.layers.filter((layer) => getFilteredIdList(getMenu()).includes(layer.id));
 };
 
 /**
  * 指定したidのconfigを取得する
  */
 export const getLayerConfigById = (id: string) => {
-  return configJson['layers'].find((layer) => layer.id === id);
+  const config = loadConfigJson();
+  return config.layers.find((layer) => layer.id === id);
 };
