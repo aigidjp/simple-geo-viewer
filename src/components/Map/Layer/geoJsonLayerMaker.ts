11121087ce4be3d602622ef869c3efc7f0e65c56
DevKoukiMaeda <devkoukimaeda@gmail.com>
2022-06-29 16:06:13 +0900

    AEDレイヤーを表示する修正

--- src/components/Map/Layer/geoJsonLayerMaker.ts
+++ src/components/Map/Layer/geoJsonLayerMaker.ts
@@ -26,9 +26,11 @@ export function makeGeoJsonLayers(
 ) {
   const geoJsonLinePolygonCreator = new GeoJsonLinePolygonCreator(layerConfig, map, setTooltipData);
   const geoJsonIconCreator = new GeoJsonIconLayerCreator(layerConfig, map, setTooltipData);
+  const geoJsoneatureCollectionIconCreator = new GeoJsonFeatureCollectionIconLayerCreator(layerConfig, map, setTooltipData);
   const layers = [
     ...geoJsonLinePolygonCreator.makeDeckGlLayers(init),
     ...geoJsonIconCreator.makeDeckGlLayers(init),
+    ...geoJsoneatureCollectionIconCreator.makeDeckGlLayers(init)
   ];
   return layers;
 }
@@ -109,6 +111,88 @@ class GeoJsonIconLayerCreator {
         autoHighlight: true,
         onClick: this.showToolTip,
         pointType: 'icon',
+        getIcon: (_) => ({
+          url: layerConfig.icon.url,
+          width: layerConfig.icon.width,
+          height: layerConfig.icon.height,
+          anchorY: layerConfig.icon.anchorY,
+          mask: false,
+        }),
+        ...config,
+      });
+    });
+
+    return result;
+  }
+
+  extractLayerConfig = (layerConfig: GeojsonIconLayerConfig) => {
+    const { type, source, ...otherConfig } = layerConfig;
+    return otherConfig;
+  };
+
+  extractTargetConfig() {
+    return this.layerConfig.filter((layerConfig: LayerConfig) => {
+      return layerConfig.type === this.layersType;
+    }) as GeojsonIconLayerConfig[];
+  }
+
+  showToolTip = (info: PickInfo<any>) => {
+    const { coordinate, object } = info;
+    if (!coordinate) return;
+    if (!object) return;
+    show(object, coordinate[0], coordinate[1], this.map, this.setTooltipData);
+  };
+}
+
+async function getJsonFeatures(url:string,filterFunc:(any)=>any = (_)=>{return _}):Promise<any>{
+  const respons  = await fetch(url);
+  const jsonData = await respons.json();
+  const features = jsonData.map(filterFunc);
+  return features;
+}
+
+class GeoJsonFeatureCollectionIconLayerCreator {
+  layersType: string = "geojsonfcicon";
+  private readonly layerConfig: LayerConfig[];
+  private readonly map: Map;
+  private readonly setTooltipData: Dispatch<SetStateAction<any>>;
+
+  constructor(layerConfig: LayerConfig[], map: Map, setTooltipData) {
+    this.layerConfig = layerConfig;
+    this.map = map;
+    this.setTooltipData = setTooltipData;
+  }
+
+  makeDeckGlLayers(init) {
+    const targetLayerConfigs = this.extractTargetConfig();
+
+    const result: GeoJsonLayer<any>[] = targetLayerConfigs.map( (layerConfig) => {
+      const config = this.extractLayerConfig(layerConfig);
+      let features: any;
+      if (layerConfig.id=="susono-aed") {
+        const aedFiler = (feature) => {
+          const fixFeature = {
+            "type":feature["種類"],
+            "properties":feature["properties"],
+            "geometry":{
+              "type":feature["geometry"]["種類"],
+              "coordinates":feature["geometry"]["coordinates"].slice(0,2)
+            }
+          }
+          return fixFeature;
+        };
+        features = getJsonFeatures(layerConfig.source,aedFiler);
+      }else{
+        features = getJsonFeatures(layerConfig.source);
+      }
+      
+      return new GeoJsonLayer({
+        data: features,
+        visible: init,
+        pickable: true,
+        autoHighlight: true,
+        onClick: this.showToolTip,
+        pointType:"icon",
         getIcon: (_) => ({
           url: layerConfig.icon.url,
           width: layerConfig.icon.width,
